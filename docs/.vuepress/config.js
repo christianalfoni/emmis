30efec36bb512b71c1ce9cd912a420a70703cb05
Christian Alfoni <christianalfoni@gmail.com>
2018-04-15 00:12:26 +0200

    configured for netlify

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,39 @@
 module.exports = {
   title: 'Emmis',
   description: 'Create a chaining API for your application',
-  base: '/emmis'
+  dest: 'vuepress',
+  head: [
+    ['link', { rel: 'icon', href: `/logo.png` }]
+  ],
+  serviceWorker: true,
+  themeConfig: {
+    repo: 'vuejs/vuepress',
+    editLinks: true,
+    docsDir: 'docs',
+    nav: [
+      {
+        text: 'Guide',
+        link: '/guide/',
+      },
+      {
+        text: 'Config Reference',
+        link: '/config/'
+      },
+      {
+        text: 'Default Theme Config',
+        link: '/default-theme-config/'
+      }
+    ],
+    sidebar: {
+      '/guide/': [
+        {
+          title: 'Guide',
+          collapsable: false,
+          children: [
+            ''
+          ]
+        }
+      ]
+    }
+  }
 }
--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,12 @@
-# Hello Vue!
+---
+home: true
+actionText: Get Started →
+actionLink: /guide/
+footer: MIT Licensed | Copyright © 2018-present Christian Alfoni
+---
+
+#
+
+``` js
+import { ProxyChain } from 'emmis'
+```
